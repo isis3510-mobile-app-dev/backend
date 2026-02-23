fad3acd8d7b012cd22df9a336cff3cdb75d5ff6e
Maria Alejandra Angulo Mejia <ma.angulom1@uniandes.edu.co>
2026-02-22 22:55:39 -0500

    Vaccines CRUD, fix on Update pet CRUD and Postman tests

--- api/services/vaccine_service.py
+++ api/services/vaccine_service.py
@@ -7,9 +7,15 @@ def create_vaccine(data):
 def get_vaccine(vaccine_id):
     return Vaccine.objects.get(id=vaccine_id)
 
+def get_all_vaccines():
+    return Vaccine.objects.all()
+
 def update_vaccine(vaccine_id, data):
-    Vaccine.objects.filter(id=vaccine_id).update(**data)
-    return get_vaccine(vaccine_id)
+    vaccine = Vaccine.objects.get(id=vaccine_id)
+    for key, value in data.items():
+        setattr(vaccine, key, value)
+    vaccine.save()
+    return vaccine
 
 def delete_vaccine(vaccine_id):
     Vaccine.objects.filter(id=vaccine_id).delete()
--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,13 @@ from api.views.pet_views import (
     medical_history,
     vaccinations,
 )
+from api.views.vaccine_views import (
+    create_vaccine_view,
+    get_vaccine_view,
+    update_vaccine_view,
+    delete_vaccine_view,
+    list_vaccines_view,
+)
 
 urlpatterns = [
     # Pets CRUD
@@ -14,4 +21,11 @@ urlpatterns = [
     # Embedded resources
     path("pets/<str:pet_id>/medical-history/", medical_history),
     path("pets/<str:pet_id>/vaccinations/", vaccinations),
+
+    # Vaccines CRUD
+     path("vaccines/", list_vaccines_view, name="list_vaccines"),
+    path("vaccines/create/", create_vaccine_view, name="create_vaccine"),
+    path("vaccines/<str:vaccine_id>/", get_vaccine_view, name="get_vaccine"),
+    path("vaccines/<str:vaccine_id>/update/", update_vaccine_view, name="update_vaccine"),
+    path("vaccines/<str:vaccine_id>/delete/", delete_vaccine_view, name="delete_vaccine"),
 ]
